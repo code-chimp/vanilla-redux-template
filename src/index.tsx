128b0329a2af2f20c87a9e299404e9240b63240f
Tim Goshinski <tim@code-chimp.com>
2022-10-23 13:36:24 -0500

    :art: Adapt to React Router 6
    - :art: switch to RR's recommended methodology for routing
    - :art: redefine `App` as a layout
    - :art: split out navigation as it always evolves out as a separate component

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap';
 import AppAlerts from './components/app/AppAlerts';
 import AppToasts from './components/app/AppToasts';
 import './styles/global.scss';
-import App from './App';
 import store from './store';
+import routes from './routes';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <AppAlerts />
-        <AppToasts />
-        <App />
-      </BrowserRouter>
+      <AppAlerts />
+      <AppToasts />
+      <RouterProvider router={routes} />
     </Provider>
   </React.StrictMode>,
 );
